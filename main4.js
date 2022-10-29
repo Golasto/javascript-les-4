1eac61561719a5b0b5b746e276252cfdc4a4bcf1
Golasto <vdgraafmatteus@gmail.com>
2022-10-29 18:30:26 +0200

    Opdracht 2 en 3 af

--- main4.js
+++ main4.js
@@ -1,5 +1,5 @@
 // VOORRAAD ARRAY MET TV'S
-const inventory = [
+const inventory4 = [
     {
         type: '43PUS6504/12',
         name: '4K TV',
@@ -161,37 +161,8 @@ const inventory = [
         sold: 8,
     },
 ];
-//Opdracht 1A
-const tvs = inventory.map((name) => {
-    return name.name
-})
-console.log(tvs)
 //----------------------------------------------------------------------------------------//
-//opdracht 1B
-const runOut = inventory.filter((sold) => {
-    return sold.originalStock < 6
-})
-//Ik wist niet welke methode goed zou zijn dus heb ik er 2 variante gemaakt.
-console.log(runOut)
-const runOut1 = inventory.map((sold) => {
-    return "Er zijn " + sold.sold + ' verkocht van de ' + sold.name +  ' en er waren er ' + sold.originalStock + ' in stock'
-})
-console.log(runOut1)
-//----------------------------------------------------------------------------------------//
-//opdracht 1C
-inventory.map((ambi) => {
-    if (ambi.options.ambiLight === true) {
-        console.log("Deze " + ambi.name + " Heeft Ambilight")
-    } else {
-        console.log("Deze " + ambi.name + " Heeft geen Ambilight")
-    }
-});
-//----------------------------------------------------------------------------------------//
-//opdracht 1D
-function sortPrice() {
-    const price = inventory.sort( (a, b) =>{
-        return parseFloat(a.price) - parseFloat(b.price)
-    })
-    console.log(sortPrice())
-}
 
+//----------------------------------------------------------------------------------------//
+
+//----------------------------------------------------------------------------------------//
